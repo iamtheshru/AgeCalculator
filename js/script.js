8778f771cd8f7d20f29a72cd14927a236fed2fd2
iamtheshru <s.j.patel.3110@gmail.com>
2025-01-25 21:00:58 +0530

    data

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+var input = document.getElementById("date");
+input.max = new Date().toISOString().split("T")[0];
+
+let result = document.getElementById("result");
+
+function calculateAge() {
+    let birthday = new Date(input.value);
+
+    let d1 = birthday.getDate();
+    let m1 = birthday.getMonth() + 1;
+    let y1 = birthday.getFullYear();
+
+    let today = new Date();
+
+    let d2 = today.getDate();
+    let m2 = today.getMonth() + 1;
+    let y2 = today.getFullYear();
+
+    let d3, m3, y3;
+
+    y3 = y2 - y1;
+
+    if (m2 >= m1) {
+        m3 = m2 - m1;
+    } else {
+        y3--;
+        m3 = 12 + m2 - m1;
+    }
+
+    if (d2 >= d1) {
+        d3 = d2 - d1;
+    } else {
+        m3--;
+        if (m3 < 0) {
+            m3 = 11;
+            y3--;
+        }
+        d3 = new Date(y1, m1, 0).getDate() + d2 - d1;
+    }
+
+    result.innerHTML = `You are <span>${y3}</span> years, <span>${m3}</span> months, and <span>${d3}</span> days old.`;
+}
